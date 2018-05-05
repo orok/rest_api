f658c4419a9d43c2c74515d50c061b5212842e30
Orokita Ukpong <orokitaukpong@OrokMBP.home>
2018-05-05 16:20:51 -0400

    Minor changes to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+/**
+ * Primary File for API
+ * 
+ */
+
+// Dependencies 
+var http = require('http');
+
+// Server should respond to all request with a string
+var server = http.createServer(function(req,res){
+    res.end('Hello World\n');
+});
+
+
+//Start server and have it listen on port 3000
+
+server.listen(3000, function(){
+    console.log('The server is listening on port 3000');
+});
